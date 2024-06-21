1f9118558a8103a34cd359f878784f307e520ac7
Gabriel Quirino <gabrielquirinolima39@gmail.com>
2024-06-20 21:20:59 -0300

    Was made the round and battles, using async functions, random blocks and switch

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const player1 = {
     NAME:"Goku Base",
     KI:"4",
-    LIFE:"100",
+    ULTIMATE:"7",
     STRENGTH:"4",
 }
 const player2 = {
     NAME:"Goku SSJ",
     KI:"6",
-    LIFE:"200",
+    ULTIMATE:"5",
     STRENGTH:"6",
 }
 const player3 = {
     NAME:"Goku Blue",
     KI:"10",
-    LIFE:"300",
+    ULTIMATE:"6",
     STRENGTH:"10",
 }
 const player4 = {
     NAME:"Goku Black",
     KI:"7",
-    LIFE:"250",
+    ULTIMATE:"3",
     STRENGTH:"8",
 }
 
@@ -27,4 +27,36 @@ async function rollDice(){
    return Math.floor(Math.random() *6) +1;
 }
 
+async function getRandomBlock(){
+    let random = Math.random();
+    let result 
+    
+    switch (true) {
+        case random < 0.33:
+            result = "Batalha de KI";
+            break;  
+        case random < 0.66:
+            result = "Batalha de Força";
+            break;
+        default :
+            result = "Duelo de Especiais";
+    }
+
+    return result
+}
+
+async function playBattleEngine(character1,character2){
+    for(let round =1; round <=5; round++){
+        console.log(`Batalha ${round}`);
+
+    let block = await getRandomBlock();
+    console.log(`Duelo: ${block}`);
+    }
+}
+
+(async function main(){
+    console.log(`Batalha entre ${player1.NAME} e ${player2.NAME} começando`);
+
+    await playBattleEngine(player1,player2);
+})();
 
